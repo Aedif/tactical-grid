67e32965a9d21b0419c18b607517bce26cbf6ff1
aedif <vaitkus.martynas@gmail.com>
2025-06-01 15:44:28 +0100

    - range highlight caching - improved gridless shape drawing using holes

--- scripts/rangeHighlighter.js
+++ scripts/rangeHighlighter.js
@@ -3,49 +3,101 @@ import { registerGameSystem, registerModules } from './externalSupport.js';
 import { MODULE_ID } from './utils.js';
 
 export class RangeHighlighter {
+  /**
+   * A map of unique token/range keys and highlight grid offsets
+   * When a unique token/range combination is encountered the distances and grid spaces
+   * to be highlighted are calculated and then stored and reused using this map
+   */
+  static _cachedRangeOffsets = new Map();
+
   constructor(token, ranges) {
     this.token = token;
 
     if (ranges instanceof Array) this.ranges = ranges;
     else this.ranges = [ranges];
 
-    this.ranges.map((d) => {
-      if (d.color) d.color = new PIXI.Color(d.color).toNumber();
-      if (d.lineColor) d.lineColor = new PIXI.Color(d.lineColor).toNumber();
-      return d;
+    const grid = canvas.grid;
+
+    // Row and column grid types can be combined for caching purposes
+    let gridType = grid.type;
+    if (gridType === CONST.GRID_TYPES.HEXODDR || gridType === CONST.GRID_TYPES.HEXEVENR) gridType = 10050;
+    else if (gridType === CONST.GRID_TYPES.HEXODDQ || gridType === CONST.GRID_TYPES.HEXEVENQ) gridType = 20050;
+
+    // Part of the unique key where the highlight grid offsets will be stored
+    const mainCacheKey = `${this.token.document.width}:${this.token.document.height}:${this.token.document.shape}:${gridType}`;
+
+    // Process ranges ready to be used for highlighting operations
+    this.ranges.forEach((range) => {
+      if (range.color) range.color = new PIXI.Color(range.color).toNumber();
+      if (range.lineColor) range.lineColor = new PIXI.Color(range.lineColor).toNumber();
+
+      if (gridType !== CONST.GRID_TYPES.GRIDLESS) {
+        // Full unique key to store highlight grid offsets within
+        range.cacheKey = `${mainCacheKey}:${range.range}`;
+
+        // Get the grid space shape and shrink it
+        const vertices = grid.getShape();
+        vertices.forEach((v) => {
+          v.x = v.x * (range.shrink ?? 0.8) + grid.sizeX / 2;
+          v.y = v.y * (range.shrink ?? 0.8) + grid.sizeY / 2;
+        });
+
+        range.shape = vertices;
+      }
     });
 
+    // If the token already has an instance of RangeHighlighter lets swap out with it
     if (this.token._tgRange) {
       this._transferRanges(this.token._tgRange);
     }
     this.token._tgRange = this;
+
+    // Sort ranges from shortest to longest
+    // This is crucial for certain operations
     this.ranges = ranges.sort((r1, r2) => r1.range - r2.range);
 
+    // Add a highlight layer to be used for drawing highlights
     canvas.interface.grid.addHighlightLayer(this.highlightId);
+
+    // Immediately perform grid highlighting
     this.highlight();
   }
 
-  _transferRanges(rh) {
-    for (const range of rh.ranges) {
+  /**
+   * Merge ranges from the provided RangeHighlighter into this one
+   * @param {RangeHighlighter} rangeHighlighter
+   */
+  _transferRanges(rangeHighlighter) {
+    for (const range of rangeHighlighter.ranges) {
       if (range.id) {
         const r = this.ranges.find((r) => r.id === range.id);
         if (!r) this.ranges.push(range);
       }
     }
 
-    rh.clear();
+    rangeHighlighter.clear();
   }
 
+  /**
+   * Unique highlight layer id specific to this token
+   */
   get highlightId() {
     return `RangeHighlighter.${this.token.document.id}`;
   }
 
+  /**
+   * Remove highlights belonging to this RangeHighlighter
+   */
   clear() {
     const hl = canvas.interface.grid.getHighlightLayer(this.highlightId);
     hl.clear();
     clearTimeout(this._timer);
   }
 
+  /**
+   * Refresh highlights.
+   * This operation is throttled for efficiency on scenes with grids
+   */
   highlight() {
     const now = new Date().getTime();
 
@@ -61,6 +113,9 @@ export class RangeHighlighter {
     }
   }
 
+  /**
+   * Highlight the grid surrounding the token based on the ranges
+   */
   _highlightGrid() {
     this._timeSinceLastHighlight = new Date().getTime();
 
@@ -79,45 +134,11 @@ export class RangeHighlighter {
       hl._tgCustomVisibility = true;
     }
 
-    //if (this.token._animation) return;
-
     hl.clear();
 
     // If we are in grid-less mode, highlight the shape directly
     if (grid.type === CONST.GRID_TYPES.GRIDLESS) {
-      this.ranges = this.ranges.sort((r1, r2) => r2.range - r1.range);
-      for (const r of this.ranges) {
-        let shape;
-
-        const tShape = this.token.document.shape;
-        const { width, height } = this.token.document.getSize();
-        const { x, y } = this.token.center;
-
-        if (tShape === CONST.TOKEN_SHAPES.ELLIPSE_1 || tShape === CONST.TOKEN_SHAPES.ELLIPSE_2) {
-          if (width === height) {
-            const radius = r.range * canvas.dimensions.distancePixels + width / 2;
-            shape = new PIXI.Circle(x, y, radius, radius);
-          } else {
-            const radiusX = r.range * canvas.dimensions.distancePixels + width / 2;
-            const radiusY = r.range * canvas.dimensions.distancePixels + height / 2;
-
-            shape = new PIXI.Ellipse(x, y, radiusX, radiusY);
-          }
-        } else {
-          const rwidth = width + r.range * 2 * canvas.dimensions.distancePixels;
-          const rheight = height + r.range * 2 * canvas.dimensions.distancePixels;
-
-          shape = new PIXI.RoundedRectangle(
-            x - rwidth / 2,
-            y - rheight / 2,
-            rwidth,
-            rheight,
-            r.range * canvas.dimensions.distancePixels
-          );
-        }
-
-        this._drawShape(hl, shape, r);
-      }
+      this._highlightGridlessShape(hl);
     }
 
     // Otherwise, highlight specific grid positions
@@ -126,105 +147,67 @@ export class RangeHighlighter {
     }
   }
 
-  _highlightGridPositions(hl) {
-    const grid = canvas.grid;
-    const d = canvas.dimensions;
+  /**
+   * Draw ranges directly as shapes. Called on gridless scenes which do not require grid space highlighting
+   * @param {GridHighlight} hl
+   */
+  _highlightGridlessShape(hl) {
+    this.ranges = this.ranges.sort((r1, r2) => r2.range - r1.range);
 
-    let { x, y } = canvas.tokens.getSnappedPoint({ x: this.token.x, y: this.token.y });
+    const shapes = this.ranges.map((r) => {
+      let shape;
 
-    const maxDistance = this.ranges[this.ranges.length - 1].range;
+      // Token shape
+      const tShape = this.token.document.shape;
+      const { width, height } = this.token.document.getSize();
+      const { x, y } = this.token.center;
 
-    const tokenHeight = Math.ceil(this.token.document.height);
-    const tokenWidth = Math.ceil(this.token.document.width);
+      if (tShape === CONST.TOKEN_SHAPES.ELLIPSE_1 || tShape === CONST.TOKEN_SHAPES.ELLIPSE_2) {
+        if (width === height) {
+          const radius = r.range * canvas.dimensions.distancePixels + width / 2;
+          shape = new PIXI.Circle(x, y, radius, radius);
+        } else {
+          const radiusX = r.range * canvas.dimensions.distancePixels + width / 2;
+          const radiusY = r.range * canvas.dimensions.distancePixels + height / 2;
 
-    const distanceV = maxDistance + Math.max(0, (1 - tokenHeight) * canvas.dimensions.distance);
-    const distanceH = maxDistance + Math.max(0, (1 - tokenWidth) * canvas.dimensions.distance);
-
-    // Get number of rows and columns
-    const snappedPoint = canvas.grid.getOffset({ x: d.width, y: d.height });
-    const maxRow = snappedPoint.i;
-    const maxCol = snappedPoint.j;
-
-    let nRows = Math.ceil((distanceH * 1.5) / d.distance / (d.size / grid.sizeY));
-    let nCols = Math.ceil((distanceV * 1.5) / d.distance / (d.size / grid.sizeX));
-    [nRows, nCols] = [Math.min(nRows, maxRow), Math.min(nCols, maxCol)];
-
-    // Get the offset of the token origin relative to the top-left grid space
-    const offset = grid.getOffset({ x, y });
-    let row0 = offset.i;
-    let col0 = offset.j;
-
-    // Identify grid coordinates roughly covered by anticipated max range
-    const hRows = row0 + nRows + tokenHeight;
-    const hCols = col0 + nCols + tokenWidth;
-
-    const tokenPositions = this._getTokenGridPositions(x, y);
-
-    if (canvas.grid.type === CONST.GRID_TYPES.SQUARE) {
-      const gs = canvas.grid.size;
-      row0 = row0 + Math.floor(this.token.document.height / 2);
-      col0 = col0 + Math.floor(this.token.document.width / 2);
-
-      const wEven = this.token.document.width % 2 === 0 ? 1 : 0;
-      const hEven = this.token.document.height % 2 === 0 ? 1 : 0;
-
-      for (let r = row0; r < hRows; r++) {
-        for (let c = col0; c < hCols; c++) {
-          const pos = { x: c * gs, y: r * gs };
-
-          let withinRange;
-          for (let j = 0; j < this.ranges.length; j++) {
-            const range = this.ranges[j];
-            const measureDistance = range.measureDistance ?? canvas.grid.measurePath.bind(canvas.grid);
-            for (let i = 0; i < tokenPositions.length; i++) {
-              let cd = measureDistance([tokenPositions[i], pos], { gridSpaces: true }).distance;
-
-              if (cd <= range.range) {
-                this._highlightGridPosition(hl, pos, range);
-
-                // Mirror, 2 lines of symmetry
-                this._highlightGridPosition(hl, { x: (col0 - (c - col0) - wEven) * gs, y: pos.y }, range);
-
-                this._highlightGridPosition(
-                  hl,
-                  { x: (col0 - (c - col0) - wEven) * gs, y: (row0 - (r - row0) - hEven) * gs },
-                  range
-                );
-
-                this._highlightGridPosition(hl, { x: pos.x, y: (row0 - (r - row0) - hEven) * gs }, range);
-
-                withinRange = range;
-                break;
-              }
-            }
-            if (withinRange) break;
-          }
+          shape = new PIXI.Ellipse(x, y, radiusX, radiusY);
         }
-      }
-    } else {
-      for (let r = row0 - nRows; r < hRows; r++) {
-        for (let c = col0 - nCols; c < hCols; c++) {
-          const pos = grid.getTopLeftPoint({ i: r, j: c });
+      } else {
+        const rwidth = width + r.range * 2 * canvas.dimensions.distancePixels;
+        const rheight = height + r.range * 2 * canvas.dimensions.distancePixels;
 
-          let withinRange;
-          for (let j = 0; j < this.ranges.length; j++) {
-            const range = this.ranges[j];
-            for (let i = 0; i < tokenPositions.length; i++) {
-              let cd = canvas.grid.measurePath([tokenPositions[i], pos]).distance;
-
-              if (cd <= range.range) {
-                this._highlightGridPosition(hl, pos, range);
-                withinRange = range;
-                break;
-              }
-            }
-            if (withinRange) break;
-          }
-        }
+        shape = new PIXI.RoundedRectangle(
+          x - rwidth / 2,
+          y - rheight / 2,
+          rwidth,
+          rheight,
+          r.range * canvas.dimensions.distancePixels
+        );
       }
+
+      return { shape, style: r };
+    });
+
+    for (let i = 0; i < shapes.length; i++) {
+      const { shape, style } = shapes[i];
+
+      // If this is not the first range that has been draw, carve out a hole to draw this range within
+      if (i != 0) {
+        hl.beginHole();
+        hl.drawShape(shape);
+        hl.endHole();
+      }
+
+      this._drawShape(hl, shape, style);
     }
   }
 
+  /**
+   * Draw a simple shape on the highlight layer
+   * @param {GridHighlight} hl
+   * @param {PIXI.Rectangle|PIXI.Circle|PIXI.Ellipse|PIXI.RoundedRectangle} shape
+   * @param {object} style
+   */
   _drawShape(hl, shape, { color, alpha = 0.1, lineColor, lineAlpha = 0.3, lineWidth = 2 } = {}) {
     if (!color && !lineColor) color = 0xffffff;
 
@@ -234,73 +217,140 @@ export class RangeHighlighter {
     hl.drawShape(shape).endFill();
   }
 
-  _highlightGridPosition(
-    layer,
-    { x, y } = {},
-    { color, alpha = 0.1, shape, shrink = 0.8, lineColor, lineWidth = 2, lineAlpha = 0.3 } = {}
-  ) {
-    if (!layer.highlight(x, y)) return;
+  /**
+   * Calculate distances and highlight grid offsets.
+   * @returns
+   */
+  _cacheRanges() {
+    // Identify ranges that have not been cached yet
+    const toCacheRanges = this.ranges.filter((range) => {
+      if (!range.cache) range.cache = RangeHighlighter._cachedRangeOffsets.get(range.cacheKey);
+      return !range.cache;
+    });
+    if (!toCacheRanges.length) return;
 
-    const vertices = canvas.grid.getShape();
-    vertices.forEach((v) => {
-      v.x = v.x * shrink + x + canvas.grid.size / 2;
-      v.y = v.y * shrink + y + canvas.grid.size / 2;
+    toCacheRanges.forEach((range) => {
+      range.cache = [];
+      RangeHighlighter._cachedRangeOffsets.set(range.cacheKey, range.cache);
     });
 
-    shape = new PIXI.Polygon(vertices);
+    // Calculate grid offsets for un-cached ranges
+    const ranges = toCacheRanges;
 
-    // shape = new PIXI.Polygon(
-    //   canvas.grid.grid.getPolygon(
-    //     x + offsetX / 2,
-    //     y + offsetY / 2,
-    //     Math.ceil(canvas.grid.sizeX) - offsetX,
-    //     Math.ceil(canvas.grid.sizeY) - offsetY
-    //   )
-    // );
+    // Determine the estimated search space using the max range
+    const maxRange = (ranges[ranges.length - 1].range / canvas.dimensions.distance) * canvas.dimensions.size;
+    const bounds = this.token.bounds;
 
-    if (Number.isFinite(color)) layer.beginFill(color, alpha);
-    if (Number.isFinite(lineColor)) layer.lineStyle(lineWidth, lineColor, lineAlpha);
-    else layer.lineStyle(0);
-    layer.drawShape(shape).endFill();
+    const maxBounds = new PIXI.Rectangle(
+      bounds.x - maxRange,
+      bounds.y - maxRange,
+      bounds.width + maxRange * 2,
+      bounds.height + maxRange * 2
+    );
+
+    if (CONFIG.debug.atg) {
+      canvas.controls.debug.clear();
+      canvas.controls.debug.lineStyle(2, 0xffffff, 1.0).drawShape(maxBounds);
+    }
+
+    // Top-left and bottom-right grid offsets within the max bounds
+    const [i0, j0, i1, j1] = canvas.grid.getOffsetRange(maxBounds);
+
+    // Get all the grid spaces occupied by the token
+    const occupied = new Set();
+    this.token.document.getOccupiedGridSpaceOffsets().forEach((offset) => occupied.add(pack(offset, offset)));
+
+    // Find the edge of the occupied grid spaces
+    // Distance measurements will be limited to these
+    const edge = new Set();
+    for (let offset of occupied) {
+      offset = unpack(offset);
+
+      for (let adj of canvas.grid.getAdjacentOffsets(offset)) {
+        if (!occupied.has(pack(adj))) {
+          edge.add(pack(offset));
+          break;
+        }
+      }
+    }
+
+    // Get the coordinates of the top-left most occupied grid space
+    // Grid highlight offsets wil be based off of this
+    const { x, y } = canvas.grid.getTopLeftPoint(this.token.document._positionToGridOffset());
+
+    // Measure grid distances within the max-bounds and cache spaces within ranges
+    for (let i = i0; i <= i1; i++) {
+      for (let j = j0; j <= j1; j++) {
+        // Ignore token occupied spaces
+        if (occupied.has(pack({ i, j }))) continue;
+
+        // Find shortest distance to this grid space from the edge of the token
+        let shortest = 99999999999999999;
+        for (const t of edge) {
+          let cd = canvas.grid.measurePath([unpack(t), { i, j }]).distance;
+          if (cd < shortest) shortest = cd;
+        }
+
+        // Store the grid space within the caches of ranges it is within
+        for (let r = 0; r < ranges.length; r++) {
+          const range = ranges[r];
+          if (shortest <= range.range) {
+            const { x: x0, y: y0 } = canvas.grid.getTopLeftPoint({ i, j });
+            range.cache.push({ x: (x0 - x) / canvas.grid.size, y: (y0 - y) / canvas.grid.size });
+          }
+        }
+      }
+    }
   }
 
-  _getTokenGridPositions(tx, ty) {
-    if (!canvas.grid.isSquare) {
-      const offsets = this.token.document.getOccupiedGridSpaceOffsets();
-      return offsets.map((offset) => canvas.grid.getTopLeftPoint(offset));
-    } else {
-      // For squares we want to optimize by only returning the outer grid squares to measure from
+  /**
+   * Highlights grid positions within the ranges
+   * @param {GridHighlight} hl
+   */
+  _highlightGridPositions(hl) {
+    this._cacheRanges();
 
-      // Get the top-left grid offset
-      const { i: i0, j: j0 } = this.token.document._positionToGridOffset();
+    const pack = function ({ x, y } = {}) {
+      return (((x + 32768) << 16) >>> 0) | ((y + 32768) & 0xffff);
+    };
 
-      // Round width and height to nearest multiple of 0.5
-      const width = Math.round(this.token.document.width * 2) / 2;
-      const height = Math.round(this.token.document.height * 2) / 2;
+    const { x, y } = canvas.grid.getTopLeftPoint(this.token.document._positionToGridOffset());
 
-      const i1 = i0 + Math.ceil(height);
-      const j1 = j0 + Math.ceil(width);
+    const highlighted = new Set();
+    for (const range of this.ranges) {
+      for (const offset of range.cache) {
+        const point = { x: x + offset.x * canvas.grid.size, y: y + offset.y * canvas.grid.size };
 
-      const offsets = [];
-
-      // Top
-      for (let i = i0; i < i1; i++) {
-        offsets.push({ i, j: j0 });
+        const packed = pack(point);
+        if (!highlighted.has(packed)) {
+          console.log(point);
+          this._highlightGridPosition(hl, point, range);
+          highlighted.add(packed);
+        }
       }
-
-      // Bottom
-      for (let i = i0; i < i1; i++) {
-        offsets.push({ i, j: j1 - 1 });
-      }
-
-      // Left/Right
-      for (let j = j0 + 1; j < j1 - 1; j++) {
-        offsets.push({ i: i0, j });
-        offsets.push({ i: i1 - 1, j });
-      }
-
-      return offsets.map((offset) => canvas.grid.getTopLeftPoint(offset));
     }
+  }
+
+  /**
+   * Highlights grid position using provided shape and style
+   * @param {*} layer
+   * @param {*} param1
+   * @param {*} param2
+   * @returns
+   */
+  _highlightGridPosition(layer, { x, y } = {}, { color, alpha = 0.1, shape } = {}) {
+    if (!layer.highlight(x, y)) return;
+
+    if (Number.isFinite(color)) layer.beginFill(color, alpha);
+    layer
+      .drawShape(
+        new PIXI.Polygon(
+          shape.map((p) => {
+            return { x: p.x + x, y: p.y + y };
+          })
+        )
+      )
+      .endFill();
   }
 }
 
@@ -492,4 +542,16 @@ async function _tokensAndItemFromItemUuid(uuid) {
   else tokens = item.actor?.getActiveTokens(true) || [];
 
   return { tokens, item };
+}
+
+const OFFSET = 32768; // Supports coordinates from -32768 to +32767
+
+function pack(offset) {
+  return (((offset.i + OFFSET) << 16) >>> 0) | ((offset.j + OFFSET) & 0xffff);
+}
+
+function unpack(packed) {
+  const i = (packed >>> 16) - OFFSET;
+  const j = (packed & 0xffff) - OFFSET;
+  return { i, j };
 }
